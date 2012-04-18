309ca2efe0cbc2c0cf3e0a7e5b95bd5def06714c
Sandro Sonntag <sso@adorsys.de>
2012-04-18 18:27:54 +0200

    json response

--- jax.server/src/main/java/de/jax/server/rest/LoginResource.java
+++ jax.server/src/main/java/de/jax/server/rest/LoginResource.java
@@ -7,6 +7,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -22,6 +24,7 @@ public class LoginResource {
 
 	
 	@POST
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response login(Login login) {
 		Registration reg = em.find(Registration.class, login.getUsername());
 		if (reg != null && reg.getPassword().equals(login.getPassword())) {
